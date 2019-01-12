6fe10e68fd70732a7f0c3830c96d1ef2172e5871
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-01-13 00:38:31 +0300

    [JS] Fix path import js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line
-import * as list from './front/11-sequence/01-list';
+import * as list from './front/11-sequences/01-list';
 // eslint-disable-next-line
-import * as htmlTags1 from './front/11-sequence/02-html-tags.js';
+import * as htmlTags1 from './front/11-sequences/02-html-tags';
 // eslint-disable-next-line
-import * as htmlTags2 from './front/11-sequence/03-html-tags.js';
+import * as htmlTags2 from './front/11-sequences/03-html-tags';
 // eslint-disable-next-line
-import * as htmlTags3 from './front/11-sequence/04-html-tags.js';
+import * as htmlTags3 from './front/11-sequences/04-html-tags';
 // eslint-disable-next-line
-import * as htmlTags4 from './front/11-sequence/05-html-tags.js';
+import * as htmlTags4 from './front/11-sequences/05-html-tags';
 // eslint-disable-next-line
-import * as htmlTags5 from './front/11-sequence/06-html-tags.js';
+import * as htmlTags5 from './front/11-sequences/06-html-tags';
 // eslint-disable-next-line
-import * as select from './front/11-sequence/07-select.js';
+import * as select from './front/11-sequences/07-select';
