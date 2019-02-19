17f9fcc4f0c0d656984ecca2a4a5b5ebb04b9a89
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-02-19 10:58:06 +0300

    [JS] Course #12 — add 03-game.js

--- src/scripts/front/12/02-game.js
+++ src/scripts/front/12/02-game.js
@@ -63,3 +63,29 @@ const run = (player1, player2, cards) => {
 };
 
 export default cards => (name1, name2) => run(name1, name2, cards);
+
+// Решение учителя
+// // BEGIN
+// if (health1 <= 0) {
+//   return consList(cons(car(head(log)), `${name1} был убит`), log);
+// }
+// const card = random(cards);
+// const cardName = car(card);
+// const damage = cdr(card)();
+// const newHealth = health2 - damage;
+//
+// const message = `Игрок '${name1}' применил '${cardName}'
+//   против '${name2}' и нанес урон '${damage}'`;
+// let stats;
+// // В логе игроки всегда должны быть на своих местах. Первый игрок слева, второй - справа
+// if (order === 1) {
+//   stats = cons(cons(health1, newHealth), message);
+// } else if (order === 2) {
+//   stats = cons(cons(newHealth, health1), message);
+// }
+// const newLog = consList(stats, log);
+// // Хитрость решения учителя состоит в том, что данные игроков всегда меняются местами. Это видно
+// // по вызову ниже. Параметры первого игрока становятся параметрами второго и наоборот.
+// // Такой подход позволяет упростить логику и всегда считать что атакует игрок номер 1.
+// return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, newLog);
+// // END
