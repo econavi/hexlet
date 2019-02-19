1c667e7921bde523ab7ba0c5de4563d2585c35d3
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-02-19 13:53:17 +0300

    [JS] Course #12 — add 04-game.js and 04-simpleCard.js

---
+++ src/scripts/front/12/04-simpleCard.js
@@ -0,0 +1,13 @@
+// Реализуйте интерфейс работы карты с типом SimpleCard по аналогии с типом PercentCard. Второй параметр у конструктора - урон.
+// simpleCard.make('Жесткий ломатель мироздания', 6);
+
+import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
+import { attach, contents } from './type'; // eslint-disable-line
+
+// BEGIN (write your solution here)
+export const make = (name, damage) => attach('SimpleCard', cons(name, damage));
+
+export const getName = self => car(contents(self));
+
+export const damage = self => cdr(contents(self));
+// END
