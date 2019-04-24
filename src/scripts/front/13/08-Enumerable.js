17f7a47b403e98746db78d1c9119c20016253690
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-04-25 00:17:08 +0300

    [JS] Course #13 add step 08

---
+++ src/scripts/front/13/08-Enumerable.js
@@ -0,0 +1,153 @@
+// Enumerable.js
+// Реализуйте метод where, основываясь на следующем интерфейсе:
+//
+// Функция может принимать любое количество параметров, каждый из которых может быть либо функцией, либо объектом. Для функций должна осуществляться простая фильтрация, для объектов нужно проверять соответствие элемента коллекции значениям по тем же ключам, что и в переданном объекте.
+//
+// const cars = [
+//   { brand: 'bmw', model: 'm5', year: 2014 },
+//   { brand: 'bmw', model: 'm4', year: 2013 },
+//   { brand: 'kia', model: 'sorento', year: 2014 },
+//   { brand: 'kia', model: 'rio', year: 2010 },
+//   { brand: 'kia', model: 'sportage', year: 2012 },
+// ];
+// coll = new Enumerable(cars);
+//
+// const result = coll
+//   .where(car => car.brand === 'kia')
+//   .where(car => car.year > 2011);
+//
+// result.toArray();
+// // [
+// //   { brand: 'kia', model: 'sorento', year: 2014 },
+// //   { brand: 'kia', model: 'sportage', year: 2012 },
+// // ]
+//
+// const result2 = coll.where({ brand: 'bmw' });
+// result2.toArray();
+// // [
+// //   { brand: 'bmw', model: 'm5', year: 2014 },
+// //   { brand: 'bmw', model: 'm4', year: 2013 },
+// // ]
+//
+// const result3 = coll.where({ brand: 'kia', model: 'sorento' });
+// result3.toArray();
+// // [
+// //   { brand: 'kia', model: 'sorento', year: 2014 },
+// // ]
+//
+// const result3 = coll.where({ brand: 'kia' }, {  model: 'sorento' });
+// result3.toArray();
+// // [
+// //   { brand: 'kia', model: 'sorento', year: 2014 },
+// // ]
+//
+// const result4 = coll.where({ brand: 'kia' }, car => car.year < 2013);
+// result4.toArray();
+// // [
+// //   { brand: 'kia', model: 'rio', year: 2010 },
+// //   { brand: 'kia', model: 'sportage', year: 2012 },
+// // ]
+// Подсказки
+// Извлечь ключи из объекта можно функцией Object.keys.
+// Проверка на функцию: typeof <value> === 'function'.
+// Метод every проверяет, удовлетворяют ли все элементы массива условию, заданному в передаваемой функции. Подробнее в документации.
+
+class Enumerable {
+  constructor(collection, operations) {
+    this.collection = collection;
+    this.operations = operations || [];
+  }
+
+  build(fn) {
+    return new Enumerable(this.collection.slice(), this.operations.concat(fn));
+  }
+
+  // BEGIN (write your solution here)
+  where(...args) {
+    const newOps = this.operations.slice();
+
+    args.map((currentArg) => {
+      if (typeof currentArg === 'function') {
+        newOps.push(coll => coll.filter(currentArg));
+      } else {
+        const objKeys = Object.keys(currentArg);
+        const init = (car) => {
+          const result = objKeys.reduce((acc, elem) => {
+            const res = (currentArg[elem] === car[elem]);
+            return res;
+          }, false);
+          return result;
+        };
+
+        newOps.push(coll => coll.filter(init));
+      }
+    });
+
+    return this.build(newOps);
+  }
+  // END
+
+  getProcessedCollection() {
+    if (!this.memo) {
+      this.memo = this.operations.reduce((acc, func) => func(acc), this.collection);
+    }
+
+    return this.memo;
+  }
+
+  get length() {
+    return this.getProcessedCollection().length;
+  }
+
+  toArray() {
+    return this.getProcessedCollection().slice();
+  }
+}
+
+export default Enumerable;
+
+
+// Решение учителя
+// class Enumerable {
+//   constructor(collection, operations) {
+//     this.collection = collection;
+//     this.operations = operations || [];
+//   }
+//
+//   build(fn) {
+//     return new Enumerable(this.collection.slice(), this.operations.concat(fn));
+//   }
+//
+//   // BEGIN
+//   where(...predicates) {
+//     const fns = predicates.map((predicate) => {
+//       if (typeof predicate === 'function') {
+//         return coll => coll.filter(predicate);
+//       }
+//
+//       const keys = Object.keys(predicate);
+//       return coll => coll.filter(element =>
+//         keys.every(key => predicate[key] === element[key]));
+//     });
+//     return this.build(fns);
+//   }
+//   // END
+//
+//   getProcessedCollection() {
+//     if (!this.memo) {
+//       this.memo = this.operations.reduce((acc, func) => func(acc), this.collection);
+//     }
+//
+//     return this.memo;
+//   }
+//
+//   get length() {
+//     return this.getProcessedCollection().length;
+//   }
+//
+//   toArray() {
+//     return this.getProcessedCollection().slice();
+//   }
+// }
+//
+// export default Enumerable;
